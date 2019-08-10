84df1850561cda393c0ee73afe1768c635d3f944
tai2company <34208377+tai2company@users.noreply.github.com>
2019-08-11 06:30:49 +0800

    Add files via upload

---
+++ oi.js
@@ -0,0 +1,12 @@
+<script>
+
+var x = document.getElementsByClassName("data");
+var i;
+for(i=0; i<x.length; i++){
+var y = x[i].getElementsByTagName("td");
+var v =y[4].innerHTML.replace("B", "").replace("$", "")/y[5].innerHTML.replace("B", "").replace("$", "").replace("M", "");
+document.getElementById("demo").innerHTML += y[2].innerHTML+"     "+
+ 100/v+"<br>"
+;
+}
+</script>
