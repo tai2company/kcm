954efaa14da0af3d42aa1f1bc825f1bc5b8dbcc8
tai2company <34208377+tai2company@users.noreply.github.com>
2019-08-11 06:42:27 +0800

    Update oi.js

--- oi.js
+++ oi.js
@@ -5,7 +5,7 @@ var i;
 for(i=0; i<x.length; i++){
 var y = x[i].getElementsByTagName("td");
 var v =y[4].innerHTML.replace("B", "").replace("$", "")/y[5].innerHTML.replace("B", "").replace("$", "").replace("M", "");
-document.getElementById("demo").innerHTML += y[2].innerHTML+"     "+
+document.getElementById("demo").innerHTML += y[2].innerHTML+"<br>"+
  100/v+"<br>"
 ;
 }
