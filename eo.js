bd3d51404419120ff71a1974c0a70c10b86dd62c
tai2company <34208377+tai2company@users.noreply.github.com>
2019-09-01 22:24:38 +0800

    Add files via upload

---
+++ eo.js
@@ -0,0 +1,20 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "llo.txt", true);
+  xhttp.send();
+}
+ function ck() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "dc.html", true);
+  xhttp.send();
+}
